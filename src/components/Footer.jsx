121d105ad1de979715382a830a16e0ab2a897634
Eli RoCas <elir_94@hotmail.com>
2024-11-05 18:36:07 -0500

    add: General Layout, header, nav and footer componets with their styles.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,32 @@
+import { useState, useEffect } from "react";
+import "./footer.scss";
+import Nav from "./nav/Nav";
 
 const Footer = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer className="footerContainer">
+      {!isMobile ? (
+        <>
+          <p className="title">&copy; MD-KeyCode - 2024.</p>
+          <p className="groupName">Cocktail Coders</p>
+          <p className="declaimer">Todos los derechos reservados</p>
+        </>
+      ) : (
+        <Nav />
+      )}
+    </footer>
   );
 };
 

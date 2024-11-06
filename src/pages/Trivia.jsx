31bcad172f9c4805be1c75ddc993703b2d70f7f1
Eli RoCas <elir_94@hotmail.com>
2024-11-05 19:37:19 -0500

    add: trivia basic structure

--- src/pages/Trivia.jsx
+++ src/pages/Trivia.jsx
@@ -1,9 +1,21 @@
-import React from "react";
-
+import imgTest from "../assets/cocktail-trivia-1.jpg";
 const Trivia = () => {
   return (
-    <div>
-      <h1>Trivia</h1>
+    <div className="gameCard">
+      <h2>Piensa o... ¡Shot!</h2>
+      <section>
+        <img
+          src={imgTest}
+          alt="cocktail image reference"
+          className="cocktailImage"
+        />
+        <p className="hints">Category: </p>
+        <p className="hints">Ingredients: </p>
+      </section>
+      <div>
+        <button>True</button>
+        <button>False</button>
+      </div>
     </div>
   );
 };

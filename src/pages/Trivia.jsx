b696edc1e21f9dcb9e1bf1d573064e5ed028ba36
Eli RoCas <elir_94@hotmail.com>
2024-11-05 20:18:29 -0500

    add: timer

--- src/pages/Trivia.jsx
+++ src/pages/Trivia.jsx
@@ -1,20 +1,40 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./trivia.scss";
 import imgTest from "../assets/cocktail-trivia-1.jpg";
+
 const Trivia = () => {
+  const [countdown, setCountdown] = useState(15);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCountdown((prev) => prev - 1);
+    }, 1000);
+
+    if (countdown === 0) {
+      navigate("/score");
+    }
+
+    return () => clearInterval(timer);
+  }, [countdown, navigate]);
+
   return (
-    <div className="gameCard">
+    <div className="triviaContainer">
       <h2>Piensa o... ¡Shot!</h2>
-      <section>
+      <section className="gameCard">
+        <p className="timer">Time left: {countdown}</p>
         <img
           src={imgTest}
           alt="cocktail image reference"
-          className="cocktailImage"
+          className="triviaImage"
         />
         <p className="hints">Category: </p>
         <p className="hints">Ingredients: </p>
       </section>
-      <div>
-        <button>True</button>
-        <button>False</button>
+      <div className="triviaAnswers">
+        <button className="triviaButton">True</button>
+        <button className="triviaButton">False</button>
       </div>
     </div>
   );

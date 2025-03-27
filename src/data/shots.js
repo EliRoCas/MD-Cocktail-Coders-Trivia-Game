f2207b960f690e3e066f70512b75d985ede49e51
Eli RoCas <elir_94@hotmail.com>
2025-03-27 12:30:17 -0500

    change vite config

---
+++ src/data/shots.js
@@ -0,0 +1,21 @@
+const reward = [
+  "Everyone Drinks But You",
+  "Give 2 Drinks",
+  "Choose Someone for a Dare",
+  "Skip the Next Round",
+  "Order a Special Cocktail",
+  "Make a Silly Rule (Until Changed)",
+  "Immunity for 2 Rounds",
+];
+
+const penalty = [
+  "Double Shot",
+  "Triple Shot",
+  "Mystery Shot (Someone Else Prepares It)",
+  "Sing a Part of a Song",
+  "Make a Ridiculous Toast and Drink",
+  "Imitate Another Player Until Your Next Turn",
+  "Ugly Selfie in the Group Chat",
+];
+
+export default { reward, penalty };
---
+++ src/pages/Roulette.jsx
@@ -0,0 +1,10 @@
+
+const Roulette = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Roulette;

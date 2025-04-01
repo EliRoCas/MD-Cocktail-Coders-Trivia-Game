795489f3c2f17403f79cc3685c92b2e0d5e8e9af
Eli RoCas <elir_94@hotmail.com>
2025-04-01 10:14:55 -0500

    fix: logo and app name now navigate to home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,23 @@
-import "./header.scss";
+import { useNavigate } from "react-router-dom";
 import drunkOwl from "../assets/drunkOwl.jpg";
 import Nav from "./nav/Nav";
+import "./header.scss";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate("/");
+  };
   return (
     <div className="headerContainer">
-      <img className="logo" src={drunkOwl} alt="logo" loading="lazy" />
-      <h1 className="pageName">Shots Pal&apos; Desmadre</h1>
+      <img
+        className="logo"
+        src={drunkOwl}
+        alt="logo"
+        loading="lazy"
+        onClick={handleNavigate}
+      />
+      <h1 className="pageName" onClick={handleNavigate}>Shots Pal&apos; Desmadre</h1>
       <Nav className="navBar" />
     </div>
   );
